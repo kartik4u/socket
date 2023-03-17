6d8bcddae84e365e66c3e13393406ddd5f0addc7
narinder.singh <narinder.singh@tensorgo.com>
2023-03-17 20:54:15 +0530

    setup

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = process.env.PORT || 3000;
+var allData = [];
+//require('./src/config/database')
+//const user_routes = require('./src/user/users.routes');
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+//app.use('/User', user_routes)
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+
+  socket.on('send_message',(data)=>{
+    console.log("received message in server side",data);
+    //allData.push(data);
+    io.emit('received_message',data[0])
+  })
+
+
+  // socket.on('update_message',(data)=>{
+  //   //console.log("received message in server side",data);
+  //   //allData.push(data);
+  //   allData = data;
+  //   io.emit('received_message',allData)
+  // })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  
+});
+
+server.listen(port, () => {
+  console.log( `Server running at http://localhost:${port}/`);
+});
