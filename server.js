9a26701fc34adffcc3d26aeb8493edc98efe2654
narinder.singh <narinder.singh@tensorgo.com>
2023-03-18 08:06:53 +0530

    changes in package.json

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const port = process.env.PORT || 3000;
+const port = "0.0.0.0";
+//process.env.PORT || 3000;
 var allData = [];
 //require('./src/config/database')
 //const user_routes = require('./src/user/users.routes');
