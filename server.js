30af70303661f4715b2fe40287e5575a62a40dfc
narinder.singh <narinder.singh@tensorgo.com>
2023-03-27 21:05:15 +0530

    new chnages room

--- server.js
+++ server.js
@@ -20,13 +20,35 @@ app.get('/', (req, res) => {
 
 //app.use('/User', user_routes)
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  //socket.emit('create', 'room1');
 
+  console.log('a user connected');
+      socket.on('create', function(room) {
+        console.log("room",room);
+         socket.join(room);
+      });
+
+      //allClients.push(socket.id);
+      //setOnline(allClients.length);
+      // socket.once('disconnect', function() {
+      //     console.log('Got disconnect!');
+
+      //     var i = allClients.indexOf(socket.id);
+      //     allClients.splice(i, 1);
+      //     setOnline(allClients.length);
+      // });
+
+
+    //  io.emit("online", allClients);
+
+    //  io.sockets.in(room).emit('event', data);
 
   socket.on('send_message',(data)=>{
     console.log("received message in server side",data);
     //allData.push(data);
-    io.emit('received_message',data)
+    //io.emit('received_message',data[0])
+    io.sockets.in(data.room).emit('received_message',data);
+
   })
 
 
@@ -37,7 +59,11 @@ io.on('connection', (socket) => {
   //   io.emit('received_message',allData)
   // })
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', (socket) => {    
+    // var i = allClients.indexOf(socket.id);
+    // allClients.splice(i, 1);
+    // io.emit("online", allClients);
+
     console.log('user disconnected');
   });
   
